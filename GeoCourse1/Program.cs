50ebc03087001dd404913c39ec2261b3e5a6075d
wu yang <1142566718@qq.com>
2022-10-31 20:26:03 +0800

    1

--- GeoCourse1/Program.cs
+++ GeoCourse1/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 //Author:WuYang
 //First_Edit_Time:2022-10-31 18:51
-//Last_Edit_Time:2022-10-31 19:35
+//Last_Edit_Time:2022-10-31 20:25
 
 //手动输入控制点的点名、高程、XY坐标，然后按照规定的格式输出这些值。
 //PName-点名;
@@ -21,16 +21,16 @@ namespace GeoCourse1
         static void Main(string[] args)
         {
             //Input
-            Console.WriteLine("请输入点号");
+            Console.WriteLine("请输入点号:");
             string PName = Console.ReadLine();
-            Console.WriteLine("请输入高程");
+            Console.WriteLine("请输入高程:");
             double Elevation = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("请输入X坐标");
+            Console.WriteLine("请输入X坐标:");
             double X = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("请输入Y坐标");
+            Console.WriteLine("请输入Y坐标:");
             double Y = Convert.ToDouble(Console.ReadLine());
             //Output
-            Console.WriteLine("已知点{0}的高程为{1}m，坐标为({2},{3})", PName, Elevation, X, Y);
+            Console.WriteLine("(1)已知点{0}的高程为:{1}m\n(2)已知点{2}坐标为:\nX={3}\nY={4}", PName, Elevation, PName,X,Y);
             Console.ReadKey();
         }
     }
