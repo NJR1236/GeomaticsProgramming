ed4e244a98f16c6f61034964323b43974d278885
wu yang <1142566718@qq.com>
2022-11-02 23:29:03 +0800

    Upload GeoCourse2

---
+++ GeoCourse2/LevelingCalculation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelingCalculation
+{
+    class LC
+    {
+        public string KnPName;      //定义已知点点名
+        public string UnPName;      //定义未知点点名
+        public double UnPElev;      //定义未知点高程
+        public double KnPElev;      //定义已知点高程
+        public double deltaElev;     //定义高差   
+
+        //定义计算未知点高程的方法
+        public double KPE(double @KnPElev, double @deltaElev)
+        {
+            double @UnPElev = @KnPElev + @deltaElev;
+            return @UnPElev;
+        }
+    }
+}
---
+++ GeoCourse2/LevelingMeasurement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LevelingCalculation;      //引用LevelingCalculation Namespace
+
+//Author:WuYang
+//First_Edit_Time:2022-11-02 21:45
+//Last_Edit_Time:2022-11-02 23:27
+
+//手动输入已知点的点名和高程与未知点的点名，再通过观测高差，计算出未知点的高程并输出。
+//LM.KnPName-已知点点名
+//LM.UnPName-未知点点名
+//LM.UnPElev-未知点高程
+//LM.KnPElev-已知点高程
+//LM.deltaElev-高差
+
+namespace GC2.LevelingMeasurement
+{
+    class LevelingMeasurement
+    {
+        static void Main(string[] args)
+        {
+            //调用LC类并new一个对象
+            LC LM = new LC();
+            Console.WriteLine("请输入已知点点名：");
+            LM.KnPName = Console.ReadLine();
+            Console.WriteLine("请输入未知点点名：");
+            LM.UnPName = Console.ReadLine();
+            Console.WriteLine("请输入已知点高程：");
+            LM.KnPElev = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入观测高差：");
+            LM.deltaElev = Convert.ToDouble(Console.ReadLine());
+            //调用LC中KPE方法，计算未知点高程
+            LM.UnPElev = LM.KPE(LM.KnPElev, LM.deltaElev);
+            Console.WriteLine("{0}点的高程为：{1}m", LM.UnPName, LM.UnPElev);
+            Console.ReadKey();
+        }
+    }
+}
